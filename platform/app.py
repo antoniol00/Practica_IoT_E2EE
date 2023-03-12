8f955c57ab02d6a2915b9d082cf215b10978101a
angel-dan-at-892988686872 <angelgg0700@gmail.com>
2023-03-12 11:49:36 +0100

    Version estable

--- platform/app.py
+++ platform/app.py
@@ -9,8 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 with app.app_context():
     db = SQLAlchemy(app)
 
+
 class Device(db.Model):
-    __tablename__ = 'device' # nombre de la tabla en minúsculas
+    __tablename__ = 'device'  # nombre de la tabla en minúsculas
     id = db.Column(db.String(200), primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     location = db.Column(db.String(200), nullable=False)
@@ -20,7 +21,7 @@ class Device(db.Model):
     hash_algorithm = db.Column(db.String(10), nullable=False)
     dh_algorithm = db.Column(db.String(10), nullable=False)
     date_register = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
     def __repr__(self):
         return '<Device %r>' % self.nombre
 
@@ -34,12 +35,50 @@ class Device(db.Model):
         self.hash_algorithm = hash_algorithm
         self.dh_algorithm = dh_algorithm
 
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(200), nullable=False)
+    time = db.Column(db.DateTime, default=datetime.now)
+    device_id = db.Column(db.String(200), db.ForeignKey(
+        'device.id'), nullable=False)
+    device = db.relationship(
+        'Device', backref=db.backref('messages', lazy=True))
+
+    def __repr__(self):
+        return '<Message %r>' % self.message
+
+    def __init__(self, message, device):
+        self.message = message
+        self.device = device
+
+
 @app.route('/')
 def index():
     devices = Device.query.all()
     return render_template('index.html', devices=devices)
 
+
+@app.route('/eliminar_dispositivo/<string:device_id>', methods=['POST'])
+def eliminar_dispositivo(device_id):
+    device = Device.query.get(device_id)
+    if device:
+        db.session.delete(device)
+        db.session.commit()
+    return redirect('/')
+
+
+@app.route('/informacion_dispositivo/<string:device_id>', methods=['GET'])
+def informacion_dispositivo(device_id):
+    messages = Message.query.filter_by(device_id=device_id).order_by(Message.time.desc())
+    device = Device.query.get(device_id)
+    # lógica para obtener la información del dispositivo correspondiente al ID
+    return render_template('informacion_dispositivo.html', messages=messages, device=device)
+
+
 if __name__ == "__main__":
-    mqtt_client_process = subprocess.Popen([sys.executable, 'platform_mqtt.py'])
+    mqtt_client_process = subprocess.Popen(
+        [sys.executable, 'platform_mqtt.py'])
     app.run(debug=False)
     mqtt_client_process.terminate()
--- platform/platform_mqtt.py
+++ platform/platform_mqtt.py
@@ -1,14 +1,3 @@
-from app import *
-from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import Encoding, ParameterFormat, PublicFormat, load_pem_public_key
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.asymmetric import dh, ec
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, modes
-from cryptography.hazmat.primitives.hmac import HMAC
-sys.path.append("..")  # Adds higher directory to python modules path.
-from encryption import encryption_algorithms, hashes_algorithms, aead_algorithms
 import base64
 import os
 from dotenv import load_dotenv
@@ -18,10 +7,23 @@ import sys
 import threading
 import time
 from datetime import datetime, timedelta
+sys.path.append("..")  # Adds higher directory to python modules path.
+from encryption import encryption_algorithms, hashes_algorithms, aead_algorithms
+from cryptography.hazmat.primitives.hmac import HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh, ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.serialization import Encoding, ParameterFormat, PublicFormat, load_pem_public_key
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidSignature
+from app import *
 
 load_dotenv()  # load environment variables from .env file
 
 # custom json encoder to encode bytes to hex
+
+
 class BytesEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, bytes):
@@ -57,7 +59,7 @@ class Platform:
         # topics where the platform will receive data from devices
         client.subscribe("GrupoB/data/*", qos=1)
 
-    def decrypt_aead(self, message, key):
+    def decrypt_aead(self, message, key, msg_dec):
 
         # Get algorithms
         try:
@@ -88,11 +90,17 @@ class Platform:
             bytes.fromhex(result['nonce']),
             bytes.fromhex(result['ct']),
             result['aad'].encode('utf-8')).decode('utf-8')
+        with app.app_context():
+            device = Device.query.filter_by(
+                id=msg_dec['device_id']).first()
+            message = Message(plaintext, device)
+            db.session.add(message.decode())
+            db.session.commit()
         print(f"Recieved the following encrypted message {plaintext}")
         print(
             f"Also receiced some additional unencrypted data: {result['aad']}")
 
-    def decrypt_ae(self, message, key):
+    def decrypt_ae(self, message, key, msg_dec):
 
         # Get algorithms
         try:
@@ -123,6 +131,12 @@ class Platform:
             bytes.fromhex(result['iv']))).decryptor()
         plaintext = decryptor.update(bytes.fromhex(
             result['ct'])) + decryptor.finalize()
+        with app.app_context():
+            device = Device.query.filter_by(
+                id=msg_dec['device_id']).first()
+            message = Message(plaintext, device)
+            db.session.add(message.decode())
+            db.session.commit()
         print(f"Recieved the following encrypted message: {plaintext}")
 
     def on_message(self, client, userdata, msg):
@@ -132,15 +146,19 @@ class Platform:
 
         if msg.topic.startswith("GrupoB/data"):
             with app.app_context():
-                if msg_dec['device_id'] not in self.devices or self.devices[msg_dec['device_id']]['registered'] == False or Device.query.filter_by(device_id=msg_dec['device_id']).first() == None  :
+                device = Device.query.filter_by(
+                    id=msg_dec['device_id']).first()
+                if device == None:
+                    del self.devices[msg_dec['device_id']]
+                if msg_dec['device_id'] not in self.devices or self.devices[msg_dec['device_id']]['is_registered'] == False:
                     print("Recieved data from unregistered device")
                     return
                 if msg_dec['enc'] == 'AE':
                     self.decrypt_ae(
-                        msg.payload, self.devices[msg_dec['device_id']]['aes_key'])
+                        msg.payload, self.devices[msg_dec['device_id']]['aes_key'], msg_dec)
                 elif msg_dec['enc'] == 'AEAD':
                     self.decrypt_aead(
-                        msg.payload, self.devices[msg_dec['device_id']]['aes_key'])
+                        msg.payload, self.devices[msg_dec['device_id']]['aes_key'], msg_dec)
                 else:
                     print(
                         f"Recieved data from device with unknown message type: {msg_dec['msg_type']}")
@@ -152,7 +170,7 @@ class Platform:
             elif msg_dec['msg_type'] == 'device_public_key':
                 self.send_dh_challenge(msg_dec)
             elif msg_dec['msg_type'] == 'device_challenge_response':
-                self.verify_dh_challenge_response(msg,msg_dec)
+                self.verify_dh_challenge_response(msg, msg_dec)
         else:
             ValueError(f"Unrecognized topic: {msg.topic}")
 
@@ -181,7 +199,8 @@ class Platform:
         }
 
         print("Generating new DH parameters... (this may take a while)")
-        self.generate_new_dh_parameters(msg_dec['dh_algorithm'])  # generate new DH parameters
+        self.generate_new_dh_parameters(
+            msg_dec['dh_algorithm'])  # generate new DH parameters
 
         platform_pk = self.platform_public_key.public_bytes(
             encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo)
@@ -194,9 +213,9 @@ class Platform:
             }
         else:
             data = {
-            'msg_type': 'platform_public_key',
-            'dh_parameters': base64.b64encode(self.param_bytes).decode('utf-8'),
-            'platform_public_key': base64.b64encode(platform_pk).decode('utf-8')
+                'msg_type': 'platform_public_key',
+                'dh_parameters': base64.b64encode(self.param_bytes).decode('utf-8'),
+                'platform_public_key': base64.b64encode(platform_pk).decode('utf-8')
             }
 
         self.client.publish(
@@ -213,9 +232,9 @@ class Platform:
             base64.b64decode(msg_dec['device_public_key']))
 
         if self.devices[msg_dec['device_id']]['dh_algorithm'] == 'ecdh':
-            self.devices[msg_dec['device_id']]['session_key'] = self.platform_private_key.exchange( ec.ECDH(),
-                self.devices[msg_dec['device_id']]['device_public_key'])
-        else: 
+            self.devices[msg_dec['device_id']]['session_key'] = self.platform_private_key.exchange(ec.ECDH(),
+                                                                                                   self.devices[msg_dec['device_id']]['device_public_key'])
+        else:
             # Compute the shared key
             self.devices[msg_dec['device_id']]['session_key'] = self.platform_private_key.exchange(
                 self.devices[msg_dec['device_id']]['device_public_key'])
@@ -250,7 +269,7 @@ class Platform:
         print("Message published to topic " +
               self.devices[msg_dec['device_id']]['mqtt_topic'] + ": " + str(data))
 
-    def verify_dh_challenge_response(self,msg,  msg_dec):
+    def verify_dh_challenge_response(self, msg,  msg_dec):
         # Derive a secret key using HKDF
         hkdf = HKDF(
             algorithm=hashes.SHA256(),
@@ -273,21 +292,26 @@ class Platform:
                 print('HMAC verified!')
                 with app.app_context():
                     if Device.query.filter_by(id=msg_dec['device_id']).first() is not None:
-                        device = Device.query.filter_by(id=msg_dec['device_id']).first()
+                        device = Device.query.filter_by(
+                            id=msg_dec['device_id']).first()
                         device.date_register = datetime.now()
                         db.session.commit()
                         print('Device ' + msg_dec['device_id'] +
-                        ' updated registered!')
+                              ' updated registered!')
                         print("New session key: " +
-                        str(self.devices[msg_dec['device_id']]['session_key']))
+                              str(self.devices[msg_dec['device_id']]['session_key']))
                         return
                     device = Device(msg_dec['device_id'], self.devices[msg_dec['device_id']]['name'],
-                                    self.devices[msg_dec['device_id']]['location'], self.devices[msg_dec['device_id']]['type'],
-                                    self.devices[msg_dec['device_id']]['encryption_mode'],
-                                    self.devices[msg_dec['device_id']]['encryption_algorithm'],
-                                    self.devices[msg_dec['device_id']]['hash_algorithm'],
+                                    self.devices[msg_dec['device_id']
+                                                 ]['location'], self.devices[msg_dec['device_id']]['type'],
+                                    self.devices[msg_dec['device_id']
+                                                 ]['encryption_mode'],
+                                    self.devices[msg_dec['device_id']
+                                                 ]['encryption_algorithm'],
+                                    self.devices[msg_dec['device_id']
+                                                 ]['hash_algorithm'],
                                     self.devices[msg_dec['device_id']]['dh_algorithm'])
-                    
+
                     db.session.add(device)
                     db.session.commit()
                 print('Device ' + msg_dec['device_id'] +
@@ -316,7 +340,7 @@ class Platform:
             Encoding.PEM, ParameterFormat.PKCS3)
         self.platform_private_key = self.params.generate_private_key()
         self.platform_public_key = self.platform_private_key.public_key()
-    
+
     def key_rotation(self):
         while True:
             # Get the current time
@@ -328,7 +352,8 @@ class Platform:
                     print("Key rotation for device " + dev + " is required...")
                     print("STEP 1. Update the DH parameters for device " + dev)
                     # Generate new DH parameters
-                    self.generate_new_dh_parameters(self.devices[dev]['dh_algorithm'])
+                    self.generate_new_dh_parameters(
+                        self.devices[dev]['dh_algorithm'])
                     # Send the platform public key to the device
                     platform_pk = self.platform_public_key.public_bytes(
                         encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo)
@@ -340,9 +365,9 @@ class Platform:
                     self.client.publish(
                         self.devices[dev]['mqtt_topic'], json.dumps(data), qos=1)
                     print("STEP 2. Platform public key sent to device " +
-                        dev)
+                          dev)
                     print("Message published to topic " +
-                        self.devices[dev]['mqtt_topic'] + ": " + str(data))
+                          self.devices[dev]['mqtt_topic'] + ": " + str(data))
             time.sleep(10)
 
 
